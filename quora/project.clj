7344d911d6ee7e742b6a002e1d9a2450a4cc6fbd
Timothy Chklovski <tchklovski@gmail.com>
2012-06-03 00:16:30 -0700

    add midje and lazytest

--- quora/project.clj
+++ quora/project.clj
@@ -1,3 +1,7 @@
 (defproject quora "0.1.0"
+  :repositories {"stuart" "http://stuartsierra.com/maven2"}
   :description "Quora hiring \"challenges\""
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dev-dependencies [[midje "1.4.0"]
+                     [com.stuartsierra/lazytest "1.2.3"
+                      :exclusions [swank-clojure]]])
